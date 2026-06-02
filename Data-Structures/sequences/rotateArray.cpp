852b946e836ddf24bc743312736addf48e7cda7e
Shivraj1712 <shivrajmaharaul688@gmail.com>
2026-06-02 10:48:02 +0530

    Pushing the solution of rotate array problem from leetcode

---
+++ Data-Structures/sequences/rotateArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class RotateArray{
+public:        
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin() + k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
